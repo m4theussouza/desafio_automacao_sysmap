a3590618ac22f436d181b2928a34c4ad4a3f7fff
m4theussouza@gmail.com <m4theussouza@gmail.com>
2020-11-30 14:42:26 -0300

    Modificando arquivo runner para executar todos os testes do arquivo feature

--- src/test/java/br/com/mat/runner/Runner.java
+++ src/test/java/br/com/mat/runner/Runner.java
@@ -10,10 +10,16 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
  * @author Matheus
  *
  */
+
+/**
+ * 
+ * Caso queira executar somente um teste especifico adicione tags = {"@Teste01"}
+ * Em Cucumber options
+ *
+ */
 @RunWith(Cucumber.class)
 @CucumberOptions(glue = {"br.com.mat.test", "br.com.mat.steps", "br.com.mat.utils"}, //
-		features = "src\\test\\resources\\features", //
-		tags = {"@Teste05"},
+		features = "src\\test\\resources\\features", 
 		plugin = { "pretty", "html:target/site/cucumber-pretty", "json:target/cucumber.json", "junit:target/cucumber.xml"},
 		monochrome = false)
 public class Runner extends AbstractTestNGCucumberTests {
